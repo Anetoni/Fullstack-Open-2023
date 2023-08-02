066da8a91c61ed2518000beb36a5632c5ba5bd6c
Anetoni <aho.anttoni@gmail.com>
2023-08-02 13:58:12 +0300

    completed backend

--- osa3/index.js
+++ osa3/index.js
@@ -6,65 +6,102 @@ const Person = require('./models/person')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status: :res[content-length] - :response-time ms :body'))
-app.use(cors())
 app.use(express.static('build'))
 
-morgan.token('body', function(req, res) { return JSON.stringify(req.body)})
+morgan.token('body', function(req) { return JSON.stringify(req.body)})
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world<h1>')
-})
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
 
 // get request to handle all person data
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(people => {
-        res.json(people)
-    })
+  Person.find({}).then(people => {
+    res.json(people)
+  })
 })
 
 // get specific person information with id
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(person => {
-        res.json(person)
-    })
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id).then(person => {
+    if(person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
+// delete the specified person from the database and the phonebook
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // post new person data
-app.post('/api/persons', (req, res) => {
-    const body = req.body
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
 
-    console.log(body)
+  console.log(body)
 
-    if(!body.name || !body.number) {
-        return res.status(400).json({'error': 'Name and number are required'})
-    }
+  if(!body.name || !body.number) {
+    return res.status(400).json({ 'error': 'Name and number are required' })
+  }
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    }) 
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+    .catch(error => next(error))
+})
+
+// updates person data with new number if person already exists in the phonebook
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
+
+  Person.findByIdAndUpdate(req.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
     })
+    .catch(error => next(error))
 })
-//
+
+//shows how many people are stored in the phonebook, and the current time
 app.get('/info', (req, res) => {
-    const people = Person.find({})
-    console.log("Phonebook has info for " + people.length)
-    const request_time = new Date().toString()
-    console.log(request_time)
-    res.send(`Phonebook has info for ${people.length} people<br>${request_time}` )
+  const request_time = new Date().toString()
+
+  Person.countDocuments({}).then(count => {
+    res.send(`Phonebook has info for ${count} people<br>${request_time}` )
+  })
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- osa3/models/person.js
+++ osa3/models/person.js
@@ -5,24 +5,38 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connection established')
-    })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB: ' + error.message)
-    })
+  .then(() => {
+    console.log('connection established')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB: ' + error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: [true, 'Phonenumber required'],
+    validate: {
+      validator: function(number) {
+        return (/(0([1-9]{1,2})-([0-9]{7}))|(0([0-9]{2})-([0-9]{8}))/).test(number)
+      },
+      message: 'Phonenumber doesn\'t match allowed formats'
+    }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject.__v
-        delete returnedObject.id
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject.__v
+    delete returnedObject.id
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
