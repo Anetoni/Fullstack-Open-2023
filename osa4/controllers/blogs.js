52ad46d37fc9ac6562a62985ac8746e63f78a1d2
Anetoni <aho.anttoni@gmail.com>
2023-08-28 11:48:35 +0300

    implemented GET and POST with async await

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,29 +1,26 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
 
-blogsRouter.post("/", (request, response) => {
-  const body = request.body;
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
 
-  console.log(body.title);
+  console.log('title of added blog: ', body.title)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes,
-  });
+    likes: body.likes
+  })
 
-  console.log(blog);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+  console.log('blog to be added: ', blog)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
 
 module.exports = blogsRouter;
