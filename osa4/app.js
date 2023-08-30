fc5dcf943952993da8d1663d75b7531acdf8138a
Anetoni <aho.anttoni@gmail.com>
2023-08-30 10:36:55 +0300

    added userRouter and clarified logger

--- osa4/app.js
+++ osa4/app.js
@@ -4,20 +4,21 @@ const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const middleware = require('./utils/middleware');
-const { info, error } = require("./utils/logger");
+const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+const usersRouter = require('./controllers/users')
 
 mongoose.set("strictQuery", false);
 
-info("connecting to MongoDB");
+logger.info("connecting to MongoDB");
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    info("connected to MongoDB");
+    logger.info("connected to MongoDB");
   })
-  .catch((err) => {
-    error("error connecting to MongoDB: ", err.message);
+  .catch((error) => {
+    logger.error("error connecting to MongoDB: ", error.message);
   });
 
 app.use(cors());
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
