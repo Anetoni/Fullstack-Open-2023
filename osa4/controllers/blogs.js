cdc11a2f7bc84c7f658413c2bcd0ee40b2de1607
Anetoni <aho.anttoni@gmail.com>
2023-08-31 09:51:38 +0300

    implemented token authorization

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,13 +1,29 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
+  
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid '})
+  }
+  const user = await User.findById(decodedToken.id)
 
   console.log('title of added blog: ', body.title)
 
@@ -15,11 +31,15 @@ blogsRouter.post('/', async (request, response, next) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user._id
   })
 
   console.log('blog to be added: ', blog)
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
@@ -30,5 +50,4 @@ blogsRouter.delete('/:id', async (request, response, next) => {
   response.status(204).end()
 })
 
-//blogsRouter.put('/:id', async (request, response, next))
 module.exports = blogsRouter;
