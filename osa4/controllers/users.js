8a8a5eb5bd4ea8ebae56495f9c84f4b6e9fd6c74
Anetoni <aho.anttoni@gmail.com>
2023-08-31 09:52:25 +0300

    added populate

--- osa4/controllers/users.js
+++ osa4/controllers/users.js
@@ -1,9 +1,9 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
   response.json(users)
 })
 
