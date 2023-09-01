81f96e208077eaccfbf87eab7f5e45385455165a
Anetoni <aho.anttoni@gmail.com>
2023-09-01 10:32:06 +0300

    login functionality implemented 5.1

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,35 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Login from './components/Login'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      console.log(`logging in as ${username}, password ${password}`)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +37,27 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+  if (user === null) {
+    return (
+      <div>
+        <Notification message={errorMessage}/>
+        <h2>log in to application</h2>
+        <Login username={username} password={password}
+        setUsername={setUsername} handleLogin={handleLogin} 
+        setPassword={setPassword}></Login>
+      </div>
+    )
+  } else if (user) {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>{user.name} logged in</p>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
 }
 
 export default App
---
+++ osa5/bloglist-frontend/src/components/Login.jsx
@@ -0,0 +1,15 @@
+const Login = ({ handleLogin, setUsername, setPassword, username, password }) => (
+  <form onSubmit={handleLogin}>
+    <div>
+      username
+      <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)}/>
+    </div>
+    <div>
+      password
+      <input type="text" value={password} name="Password" onChange={({ target }) => setPassword(target.value)}/>
+    </div>
+    <button type="submit">login</button>
+  </form>
+)
+
+export default Login
