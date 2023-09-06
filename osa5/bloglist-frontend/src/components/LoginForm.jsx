0ed57ca35e2f31453973c5888a7decc2f858c743
Anetoni <aho.anttoni@gmail.com>
2023-09-06 12:34:23 +0300

    input ids defined

--- osa5/bloglist-frontend/src/components/LoginForm.jsx
+++ osa5/bloglist-frontend/src/components/LoginForm.jsx
@@ -4,13 +4,13 @@ const LoginForm = ({ handleLogin, setUsername, setPassword, username, password }
   <form onSubmit={handleLogin}>
     <div>
       username
-      <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)}/>
+      <input type="text" id='username' value={username} name="Username" onChange={({ target }) => setUsername(target.value)}/>
     </div>
     <div>
       password
-      <input type="text" value={password} name="Password" onChange={({ target }) => setPassword(target.value)}/>
+      <input type="text" id='password' value={password} name="Password" onChange={({ target }) => setPassword(target.value)}/>
     </div>
-    <button type="submit">login</button>
+    <button id='login-button' type="submit">login</button>
   </form>
 )
 
