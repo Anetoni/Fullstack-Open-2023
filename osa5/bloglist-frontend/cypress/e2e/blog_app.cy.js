7479d36b1718511996b374c0bac6068c0928c3fc
Anetoni <aho.anttoni@gmail.com>
2023-09-07 10:24:05 +0300

    5.18 login tests implemented

--- osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -14,4 +14,28 @@ describe('Blog app', function() {
   it('Login form is shown', function() {
     cy.contains('log in to application')
   })
+
+  //5.18
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('mcloving')
+      cy.get('#password').type('secret')
+      cy.get('#login-button').click()
+
+      cy.contains('Testaaja McLoving logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('mcloving')
+      cy.get('#password').type('notsecret')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'Testaaja McLoving logged in')
+    })
+  })
 })
